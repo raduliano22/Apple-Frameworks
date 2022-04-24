4d2c46e2ce0f110f6d453af8d3554e220ed3c84b
raduliano <radulian@me.com>
2022-04-24 08:51:33 +0300

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Octav Radulian on 24.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
